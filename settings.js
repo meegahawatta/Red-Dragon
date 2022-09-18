ad9c576e8b4f7c49323332385be029dd08e75e27
meegahawatta <112864011+meegahawatta@users.noreply.github.com>
2022-09-18 10:29:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,7 +12,7 @@ global.APIKeys = {
 }
 
 global.alivemsg = `📤මම ඉන්නවා😇
-🤷‍♀️How cමම ඔයාට කොහොමද උදව් කල යුත්තේ😉` //Costomize Alive Message (type your message in `` )
+🤷‍♀️මම ඔයාට කොහොමද උදව් කල යුත්තේ😉` //Costomize Alive Message (type your message in `` )
 
 
 global.autoTyping = false //auto tying in gc (true to on, false to off)
